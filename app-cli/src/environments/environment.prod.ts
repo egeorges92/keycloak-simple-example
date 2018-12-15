c8f9451a0320ce7aa468ed026000be57f8c8384e
Eric Georges <eric.georges@businessdecision.com>
2018-12-15 12:37:44 +0100

    Add ssl support

--- app-cli/src/environments/environment.prod.ts
+++ app-cli/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.example.org',
+  apiUrl: 'https://api.example.org',
   keycloak: {
-    url: 'http://identity.example.org/auth',
+    url: 'https://identity.example.org/auth',
     realm: 'example',
     clientId: 'app-cli',
   }
